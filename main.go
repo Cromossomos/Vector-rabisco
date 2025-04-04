454f8f920eb25e92ac271efd692859869849e85c
Cromo <carol.simoes@aluno.senai.br>
2025-04-04 08:21:59 -0300

    Stand Libary

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
-
+import (
+"fmt" 
+"strings"
+"sort"
+)
 func main(){
 
-nomes := []string{"Enzo Gabriel", "Thiago", "Vini", "Isabella"}
-
-fmt.Println("Dois primeiros:", nomes[:2])
-fmt.Println("Dois ultimos nomes:", nomes[len(nomes)-2:])
-fmt.Println("Nomes do meio:", nomes[len(nomes)/2:len(nomes)/2+1])
-
+	greeting := "Opa meu amigos"
+	fmt.Println(strings.Contains(greeting, "amigos"))
+	fmt.Println(strings.ReplaceAll(greeting, "Opa", "Oi"))
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting, "meu"))
+	fmt.Println(strings.Split(greeting, "amigos"))
+	ages := []int {50, 80, 10}
+	sort.Ints(ages)
+	fmt.Println(ages)
+	index := sort.SearchInts(ages, 50)
+	fmt.Println(index)
+	names := []string{"Caroline", "Maicon", "Diego"}
+	sort.Strings(names)
+	fmt.Println(names)
+	fmt.Println(sort.SearchStrings(names, "Caroline"))
+	
 }
