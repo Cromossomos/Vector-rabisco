39de7458cb05555544f134531fa346b7d39d1484
Cromo <carol.simoes@aluno.senai.br>
2025-04-11 13:31:24 -0300

    saldo

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
- 
- import "fmt"
- 
- func sayGreeting(nome string){
-	fmt.Println("Olá!", nome)
- }
 
- func addNumber(numero1 int, numero2 int) int{
-	return numero1 + numero2
- }
+import ("fmt")
 
- func main() {
-	sayGreeting("Alberto")
-	resultado := addNumber (10, 20)
-	fmt.Println(resultado)
- }
+func main(){
+var saldo int 
+var numero int
+var escolha int
+
+fmt.Print("Insira o valor que você gostaria de sacar: ")
+fmt.Scan(&saldo)
+
+    fmt.Println("Escolha uma opção:")
+    fmt.Println("1 - Sacar")
+    fmt.Println("2 - Depositar")
+    fmt.Print("Opção: ")
+    fmt.Scan(&escolha)
+
+    if escolha == 1 {
+        fmt.Print("Insira o valor que você gostaria de sacar: ")
+        fmt.Scan(&numero)
+        if numero > saldo {
+            fmt.Println("Saldo insuficiente.")
+        } else {
+            saldo -= numero
+            fmt.Println("Saque realizado com sucesso!")
+        }
+    } else if escolha == 2 {
+        fmt.Print("Insira o valor que você gostaria de depositar: ")
+        fmt.Scan(&numero)
+        saldo += numero
+        fmt.Println("Depósito realizado com sucesso!")
+    } else {
+        fmt.Println("Opção inválida.")
+    }
+
+    fmt.Println("Saldo atual:", saldo)
+}
