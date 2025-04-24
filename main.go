04f045a23ad1fa69079b64dee2b3980cd230fefc
Cromo <carol.simoes@aluno.senai.br>
2025-04-24 14:17:24 -0300

    dicionario

--- main.go
+++ main.go
@@ -2,28 +2,22 @@ package main
 
 import "fmt"
 
-var nota1 float64
-var nota2 float64
-var media float64
-
-func analisarNotas (nota1, nota2 float64) (float64, string) {
-    media = (nota1 + nota2) / 2
-    if media < 0 || media > 10 {
-        fmt.Println("Nota inválida!")
-    } else if media >= 6 {
-        return media, "Aprovado"
-    } else if media < 6 {
-        return media, "Reprovado"
-    } 
-    return media, "invalido"
-}
-
 func main (){
-    fmt.Println("Digite a primeira nota:")
-    fmt.Scanln(&nota1)
-    fmt.Println("Digite a segunda nota:")
-    fmt.Scanln(&nota2)
-    media, resultado := analisarNotas(nota1, nota2)
-    fmt.Printf("A média é: %.2f\n", media)
-    fmt.Println("Resultado:", resultado)
-    }
+   capitais := map[string]string{
+      "SP" : "São Paulo",
+      "RJ" : "Rio de Janeiro",
+      "ES" : "Espirito Santo",
+      "AC" : "Acre",
+   }
+
+   capitais["BH"] = "Belo Horizonte"
+
+   for k,v := range capitais {
+      fmt.Println("Sigla, Nome", k, v)
+   }
+   delete(capitais, "AC")
+
+   for k,v := range capitais {
+      fmt.Sprintln("Sigla, Nome", k, v)
+   }
+}
